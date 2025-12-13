261148bfb4db2076cce4871e1c1a04871d754b98
Pratish <kulkarnipratish01@gmail.com>
2025-12-13 11:21:39 +0530

    The big data medium quiz has been updated

--- questions_loader.py
+++ questions_loader.py
@@ -6,7 +6,7 @@ from app import app, db, Question
 DATA_DIR = "data"
 
 FILES = [
-    ("Stats", "Easy", "Stats.json"),
+    ("Big data", "Medium", "Stats_med.json"),
     # Add more subjects & levels if needed
 ]
 
